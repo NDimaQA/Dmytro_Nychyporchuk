ae82458e5cafc71d2a3c391c374af551fc9b90be
NDimaTest <dnichiporchuk@ukr.net>
2021-12-17 17:57:42 +0200

    init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,10 @@
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/resources"}
+)
 public class Main {
 }
--- src/test/java/resourses/MyStepdefs.java
+++ src/test/java/resourses/MyStepdefs.java
@@ -1,48 +1,68 @@
 package resourses;
 
+import io.cucumber.java.bs.A;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import org.junit.Assert;
+
+import java.io.File;
 
 public class MyStepdefs {
-    @Given("I have a file {string}")
-    public void iHaveAFile(String arg0) {
 
+    private static final String FILES = "src/test/resources/files/";
+    private static final apitest.DropboxApp APP = new apitest.DropboxApp();
+    private static HttpResponse<JsonNode> lastRequest;
+
+    //UPLOAD
+    @Given("I have a file {string}")
+    public void exists(String name) {
+        File file = new File(FILES + name);
+        Assert.assertTrue("EXISTS: File " + name + " does not exist", file.exists());
     }
 
     @When("I upload file {string} to Dropbox")
-    public void iUploadFileToDropbox(String arg0) {
+    public void uploadToDropbox(String name) {
+        lastRequest = APP.uploadFile(new File(FILES + name));
     }
 
     @Then("I see file {string} successfully uploaded")
-    public void iSeeFileSuccessfullyUploaded(String arg0) {
+    public void isSuccessfullyUploaded(String name) {
+        Assert.assertTrue("UPLOAD FILE: Failed to upload file " + name + " to Dropbox",
+                APP.getLastResponse(new File(FILES + name).getAbsolutePath()).isSuccess());
     }
 
+    //GET_FILE_METADATA
+
     @Given("I have an uploaded file {string}")
-    public void iHaveAnUploadedFile(String arg0) {
+    public void isUploadedBefore(String name) {
+        File file = new File(FILES + name);
+        Assert.assertFalse("IS FILE UPLOADED: File " + file.getName() + "is not uploaded", APP.getLastResponse(file.getAbsolutePath()) == null
+                || !APP.getLastResponse(file.getAbsolutePath()).isSuccess());
     }
 
     @When("I request metadata of file {string} by its id")
-    public void iRequestMetadataOfFileByItsId(String arg0) {
+    public void requestFileMetadata(String name) {
+        lastRequest = APP.getFileMetadata(new File(FILES + name));
     }
 
     @Then("I receive metadata for file {string}")
-    public void iReceiveMetadataForFile(String arg0) {
-    }
-
-    @When("I ask for list of files and folders in {string}")
-    public void iAskForListOfFilesAndFoldersIn(String arg0) {
-    }
-
-    @Then("I receive list of files and folders")
-    public void iReceiveListOfFilesAndFolders() {
+    public void isMetadataReceived(String name) {
+        Assert.assertTrue("RECEIVE FILE METADATA: Failed to get metadata for file " + name,
+                APP.getLastResponse(new File(FILES + name).getAbsolutePath()).isSuccess());
     }
 
     @When("I ask to delete file {string}")
-    public void iAskToDeleteFile(String arg0) {
+    public void deleteFile(String name) {
+        lastRequest = APP.deleteFile(new File(FILES + name));
     }
 
     @Then("I see file {string} is successfully deleted")
-    public void iSeeFileIsSuccessfullyDeleted(String arg0) {
+    public void isDeletedSuccessfully(String name) {
+        Assert.assertTrue("DELETE FILE: Failed to delete file " + name,
+                APP.getLastResponse(new File(FILES + name).getAbsolutePath()).isSuccess());
     }
+
 }
